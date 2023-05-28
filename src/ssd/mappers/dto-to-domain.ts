f459259bf8e632dc7686b7a20a75270146e924b3
loneloon <f.kleshev@gmail.com>
2023-05-29 00:24:57 +0700

    Ssd dto to domain mappers

---
+++ src/ssd/mappers/dto-to-domain.ts
@@ -0,0 +1,41 @@
+import {
+  Session as SessionDto,
+  Secret as SecretDto,
+  Device as DeviceDto,
+} from "@prisma-dual-cli/generated/ssd-client";
+import { Secret } from "../models/secret";
+import { Session } from "../models/session";
+import { Device } from "../models/device";
+import { DateTime } from "luxon";
+import { Metadata, SourceMetadata } from "@shared/models/common";
+
+export function mapSessionDtoToDomain(sessionDto: SessionDto): Session {
+  return new Session(
+    sessionDto.id,
+    sessionDto.isActive,
+    DateTime.fromJSDate(sessionDto.expiresAt),
+    sessionDto.deviceId,
+    new Metadata(
+      DateTime.fromJSDate(sessionDto.createdAt),
+      DateTime.fromJSDate(sessionDto.updatedAt),
+      new SourceMetadata()
+    )
+  );
+}
+
+export function mapSecretDtoToDomain(secretDto: SecretDto): Secret {
+  return new Secret(
+    secretDto.userId,
+    secretDto.passHash,
+    secretDto.salt,
+    new Metadata(
+      DateTime.fromJSDate(secretDto.createdAt),
+      DateTime.fromJSDate(secretDto.updatedAt),
+      new SourceMetadata()
+    )
+  );
+}
+
+export function mapDeviceDtoToDomain(deviceDto: DeviceDto): Device {
+  return new Device(deviceDto.id, deviceDto.userAgent, deviceDto.ip);
+}
