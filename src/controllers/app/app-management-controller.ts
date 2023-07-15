c34811d1f616387be909bc43e97b9ed139f9c951
LoneLoon <f.kleshev@gmail.com>
2023-07-15 13:47:41 +0300

    R

--- src/controllers/app/app-management-controller.ts
+++ src/controllers/app/app-management-controller.ts
@@ -46,55 +46,49 @@ export class AppManagementController {
     );
   }
 
-  public async resetAccessKeys(
-    appName: string,
-    email: string,
-    backupCode: string
-  ): Promise<ApplicationKeysDto> {
+  public async getApp(appName: string): Promise<PublicApplicationViewDto> {
     const app: Application = await this.resolveAppByName(appName);
 
-    if (app.email !== email) {
-      throw new InvalidAppCredentialsError();
-    }
+    return mapApplicationDomainToPublicApplicationViewDto(app);
+  }
 
-    return await this.appSecretController.resetAccessKeys(
-      app.id,
-      app.metadata.creationTimestamp,
-      backupCode
+  public async getAllPublicApps(): Promise<PublicApplicationViewDto[]> {
+    const allApps: Application[] =
+      await this.appPersistenceService.getAllApplications();
+    const allActiveApps: Application[] = allApps.filter((app) => app.isActive);
+
+    // TechDebt: We don't have this property on application model yet,
+    // but in the future we might need to filter out the applications that choose to remain private/hidden
+
+    return allActiveApps.map((app) =>
+      mapApplicationDomainToPublicApplicationViewDto(app)
     );
   }
 
-  private async resolveAppById(appId: string): Promise<Application> {
+  public async getAppAccount(
+    accessKeyId: string,
+    secretAccessKey: string
+  ): Promise<Omit<ApplicationDto, "id" | "isActive">> {
+    const targetApp: Application = await this.resolveAppByAccessKey(
+      accessKeyId,
+      secretAccessKey
+    );
+
+    return mapApplicationDomainToPrivateApplicationViewDto(targetApp);
+  }
+
+  private async checkAppNameAvailability(name: string): Promise<boolean> {
     const app: Application | null =
-      await this.appPersistenceService.getApplicationById(appId);
+      await this.appPersistenceService.getApplicationByName(name);
 
-    if (!app) {
-      // This can happen if an application record was hard deleted but related secret wasn't deleted before/after that
-      // We can communicate that application doesn't exist anymore, but this is considered a critical error and requires manual action
-      console.warn(
-        JSON.stringify({
-          message:
-            "[CRITICAL] Orphan secret detected! There is a leftover secret record that is linked to an app that doesn't exist anymore. Cannot resolve application!",
-          appId,
-        })
-      );
-      throw new AppDoesntExistAnymoreWithAccessKeysError(appId);
+    if (app && app.isActive) {
+      return false;
+    } else if (app && !app.isActive) {
+      // TODO: There is a complicated edge-case where we need to account for
+      // previously "deleted" applications, in our logic we deactivate them instead of deleting
+      return false;
     }
-
-    if (!app.isActive) {
-      // This can happen if an application record was deactivated but related secret wasn't deleted before/after that
-      // We can communicate that application doesn't exist anymore, but this is considered a critical error and requires manual action
-      console.warn(
-        JSON.stringify({
-          message:
-            "[CRITICAL] Orphan secret detected! There is a leftover secret record that is linked to a deactivated app. Cannot resolve application!",
-          appId: appId,
-        })
-      );
-      throw new AppDoesntExistAnymoreWithAccessKeysError(appId);
-    }
-
-    return app;
+    return true;
   }
 
   public async registerApp(
@@ -155,32 +149,22 @@ export class AppManagementController {
     }
   }
 
-  public async getAllPublicApps(): Promise<PublicApplicationViewDto[]> {
-    const allApps: Application[] =
-      await this.appPersistenceService.getAllApplications();
-    const allActiveApps: Application[] = allApps.filter((app) => app.isActive);
+  public async resetAccessKeys(
+    appName: string,
+    email: string,
+    backupCode: string
+  ): Promise<ApplicationKeysDto> {
+    const app: Application = await this.resolveAppByName(appName);
 
-    // TechDebt: We don't have this property on application model yet,
-    // but in the future we might need to filter out the applications that choose to remain private/hidden
+    if (app.email !== email) {
+      throw new InvalidAppCredentialsError();
+    }
 
-    return allActiveApps.map((app) =>
-      mapApplicationDomainToPublicApplicationViewDto(app)
+    return await this.appSecretController.resetAccessKeys(
+      app.id,
+      app.metadata.creationTimestamp,
+      backupCode
     );
-  }
-
-  private async resolveAppByName(appName: string): Promise<Application> {
-    const app: Application | null =
-      await this.appPersistenceService.getApplicationByName(appName);
-
-    if (!app) {
-      throw new AppDoesntExistError(appName);
-    }
-
-    if (!app.isActive) {
-      throw new AppDoesntExistAnymoreError(app.id);
-    }
-
-    return app;
   }
 
   public async updateAppName(
@@ -217,19 +201,6 @@ export class AppManagementController {
     await this.appUpdateController.updateAppUrl(targetApp.id, url);
   }
 
-  public async resolveAppByAccessKey(
-    accessKeyId: string,
-    secretAccessKey: string
-  ): Promise<Application> {
-    const targetAppId: string =
-      await this.appSecretController.resolveAppIdByAccessKey(
-        accessKeyId,
-        secretAccessKey
-      );
-
-    return await this.resolveAppById(targetAppId);
-  }
-
   public async updateAppEmail(
     accessKeyId: string,
     secretAccessKey: string,
@@ -240,24 +211,6 @@ export class AppManagementController {
       secretAccessKey
     );
     await this.appUpdateController.updateAppEmail(targetApp.id, email);
-  }
-
-  public async getApp(appName: string): Promise<PublicApplicationViewDto> {
-    const app: Application = await this.resolveAppByName(appName);
-
-    return mapApplicationDomainToPublicApplicationViewDto(app);
-  }
-
-  public async getAppAccount(
-    accessKeyId: string,
-    secretAccessKey: string
-  ): Promise<Omit<ApplicationDto, "id" | "isActive">> {
-    const targetApp: Application = await this.resolveAppByAccessKey(
-      accessKeyId,
-      secretAccessKey
-    );
-
-    return mapApplicationDomainToPrivateApplicationViewDto(targetApp);
   }
 
   public async deactivateApp(
@@ -274,18 +227,65 @@ export class AppManagementController {
     await this.appSecretController.deleteAppSecret(targetApp.id);
   }
 
-  private async checkAppNameAvailability(name: string): Promise<boolean> {
+  private async resolveAppById(appId: string): Promise<Application> {
     const app: Application | null =
-      await this.appPersistenceService.getApplicationByName(name);
+      await this.appPersistenceService.getApplicationById(appId);
 
-    if (app && app.isActive) {
-      return false;
-    } else if (app && !app.isActive) {
-      // TODO: There is a complicated edge-case where we need to account for
-      // previously "deleted" applications, in our logic we deactivate them instead of deleting
-      return false;
+    if (!app) {
+      // This can happen if an application record was hard deleted but related secret wasn't deleted before/after that
+      // We can communicate that application doesn't exist anymore, but this is considered a critical error and requires manual action
+      console.warn(
+        JSON.stringify({
+          message:
+            "[CRITICAL] Orphan secret detected! There is a leftover secret record that is linked to an app that doesn't exist anymore. Cannot resolve application!",
+          appId,
+        })
+      );
+      throw new AppDoesntExistAnymoreWithAccessKeysError(appId);
     }
-    return true;
+
+    if (!app.isActive) {
+      // This can happen if an application record was deactivated but related secret wasn't deleted before/after that
+      // We can communicate that application doesn't exist anymore, but this is considered a critical error and requires manual action
+      console.warn(
+        JSON.stringify({
+          message:
+            "[CRITICAL] Orphan secret detected! There is a leftover secret record that is linked to a deactivated app. Cannot resolve application!",
+          appId: appId,
+        })
+      );
+      throw new AppDoesntExistAnymoreWithAccessKeysError(appId);
+    }
+
+    return app;
+  }
+
+  private async resolveAppByName(appName: string): Promise<Application> {
+    const app: Application | null =
+      await this.appPersistenceService.getApplicationByName(appName);
+
+    if (!app) {
+      throw new AppDoesntExistError(appName);
+    }
+
+    if (!app.isActive) {
+      throw new AppDoesntExistAnymoreError(app.id);
+    }
+
+    return app;
+  }
+
+  public async resolveAppByAccessKey(
+    accessKeyId: string,
+    secretAccessKey: string
+  ): Promise<Application> {
+    const targetAppId: string =
+      await this.appSecretController.resolveAppIdByAccessKey(
+        accessKeyId,
+        secretAccessKey
+      );
+
+    return await this.resolveAppById(targetAppId);
   }
 
   // ======================================
